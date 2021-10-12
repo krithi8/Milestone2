3242ee60841e5d01a4fb983ccb570e20ee9348f6
krithi8 <katty.gopi@gmail.com>
2021-10-12 13:08:51 +0530

    commiting

---
+++ src/main/java/io/sprringboot/SpringBootDemo/App.java
@@ -0,0 +1,12 @@
+package io.sprringboot.SpringBootDemo;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/io/sprringboot/SpringBootDemo/FirstSpringBoot.java
@@ -0,0 +1,13 @@
+package io.sprringboot.SpringBootDemo;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringBoot {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+SpringApplication.run(FirstSpringBoot.class,args);
+	}
+
+}
